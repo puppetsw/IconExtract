13df790982f5162b6ab752b7cfa8e22e533f31bf
Scott Whitney <79826944+puppetsw@users.noreply.github.com>
2022-02-08 10:48:22 +1030

    Cleanup

---
+++ src/IconExtract/Helpers/BitmapHelpers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+using Microsoft.UI.Xaml.Media.Imaging;
+
+namespace IconExtract.Helpers
+{
+    public static class BitmapHelpers
+    {
+        public static async Task<BitmapImage> ConvertImage(byte[] image)
+        {
+            using InMemoryRandomAccessStream ms = new();
+
+            using (DataWriter writer = new(ms.GetOutputStreamAt(0)))
+            {
+                writer.WriteBytes(image);
+                await writer.StoreAsync();
+            }
+
+            var output = new BitmapImage();
+            await output.SetSourceAsync(ms);
+            return output;
+        }
+    }
+}
--- src/IconExtract/Services/Implementation/ExtractService.cs
+++ src/IconExtract/Services/Implementation/ExtractService.cs
@@ -3,24 +3,16 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using IconExtract.Helpers;
 using IconExtract.Models;
 using IconExtract.Services.Interfaces;
-using Microsoft.UI.Xaml.Media.Imaging;
 using Windows.Storage;
-using Windows.Storage.Streams;
 
 namespace IconExtract.Services.Implementation
 {
     public class ExtractService : IExtractService
     {
-        [DllImport("shell32.dll", CharSet = CharSet.Auto)]
-        private static extern IntPtr ExtractIcon(IntPtr handle, [MarshalAs(UnmanagedType.LPWStr)] string fileName, int iconIndex);
-
-        [DllImport("user32.dll", EntryPoint = "DestroyIcon", SetLastError = true)]
-        private static extern int DestroyIcon(IntPtr icon);
-
         public async Task<IList<IconObject>> ExtractIconsFromDll(IStorageFile file)
         {
             var iconsList = new List<IconObject>();
@@ -30,20 +22,20 @@ namespace IconExtract.Services.Implementation
 
             try
             {
-                int iconCount = (int)ExtractIcon(currentProc.Handle, file.Path, -1);
+                int iconCount = (int)Win32Helpers.ExtractIcon(currentProc.Handle, file.Path, -1);
                 for (int index = 0; index < iconCount; index++)
                 {
-                    hIcon = ExtractIcon(currentProc.Handle, file.Path, index);
+                    hIcon = Win32Helpers.ExtractIcon(currentProc.Handle, file.Path, index);
 
                     using var icon = Icon.FromHandle(hIcon);
 
                     var bitmap = icon.ToBitmap();
                     var bitmapData = (byte[])new ImageConverter().ConvertTo(bitmap, typeof(byte[]));
-                    var bitmapImage = await ConvertImage(bitmapData);
+                    var bitmapImage = await BitmapHelpers.ConvertImage(bitmapData);
 
                     var iconObject = new IconObject(Path.GetFileNameWithoutExtension(file.Name), index, bitmapImage, bitmap, bitmapData);
                     iconsList.Add(iconObject);
-                    DestroyIcon(hIcon);
+                    Win32Helpers.DestroyIcon(hIcon);
                 }
             }
             catch (Exception ex)
@@ -55,26 +47,11 @@ namespace IconExtract.Services.Implementation
             {
                 if(hIcon != IntPtr.Zero)
                 {
-                    DestroyIcon(hIcon);
+                    Win32Helpers.DestroyIcon(hIcon);
                 }
             }
 
             return iconsList;
-        }
-
-        private static async Task<BitmapImage> ConvertImage(byte[] image)
-        {
-            using InMemoryRandomAccessStream ms = new();
-
-            using (DataWriter writer = new(ms.GetOutputStreamAt(0)))
-            {
-                writer.WriteBytes(image);
-                await writer.StoreAsync();
-            }
-
-            var output = new BitmapImage();
-            await output.SetSourceAsync(ms);
-            return output;
         }
     }
 }
